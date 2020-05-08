6bbd56db1a539c52f66a205fe30b8c67c420e538
YvanPB21 <43244380+YvanPB21@users.noreply.github.com>
2020-05-08 02:10:21 -0500

    Implemetation of User Profile

---
+++ Resources/SaveUserProfileResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dopta.API.Resources
+{
+    public class SaveUserProfileResource
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        public int Dni { get; set; }
+        //public DateTime DateOfBirth { get; set; }
+        //falta gender
+        [Required]
+        [MaxLength(255)]
+        public string ProfilePickUrl { get; set; }
+    }
+}
