a6edf93a69a607d53423ed4496f60cb5b01fb41b
YvanPB21 <43244380+YvanPB21@users.noreply.github.com>
2020-05-07 21:22:27 -0500

    Create Profile

---
+++ Controllers/Profile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dopta.API.Controllers
+{
+    public class Profile
+    {
+    }
+}
