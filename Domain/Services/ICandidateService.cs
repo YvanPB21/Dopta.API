918ea8a02f3bb03933d0f7793564f7a19145fcda
YvanPB21 <43244380+YvanPB21@users.noreply.github.com>
2020-05-09 18:43:03 -0500

    CandidateService

--- Domain/Services/ICandidateService.cs
+++ Domain/Services/ICandidateService.cs
@@ -1,4 +1,5 @@
 ﻿using Dopta.API.Domain.Models;
+using Dopta.API.Domain.Services.Communications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,9 @@ namespace Dopta.API.Domain.Services
     public interface ICandidateService
     {
         Task<IEnumerable<Candidate>> ListAsync();
+        Task<IEnumerable<Candidate>> ListByPostIdAsync(int postId);
+        Task<IEnumerable<Candidate>> ListByCandidateIdAsync(int canididateId);
+        Task<CandidateResponse> AssignCandidateAsync(int postId, int candidateId);
+        Task<CandidateResponse> UnassignCandidateAsync(int postId, int candidateId);
     }
 }
--- Services/CandidateService.cs
+++ Services/CandidateService.cs
@@ -1,6 +1,7 @@
 ﻿using Dopta.API.Domain.Models;
 using Dopta.API.Domain.Repositories;
 using Dopta.API.Domain.Services;
+using Dopta.API.Domain.Services.Communications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,47 @@ namespace Dopta.API.Services
         public async Task<IEnumerable<Candidate>> ListAsync()
         {
             return await _candidateRepository.ListAsync();
+        }
+
+        public async Task<IEnumerable<Candidate>> ListByPostIdAsync(int postId)
+        {
+            return await _candidateRepository.ListByPostIdAsync(postId);
+        }
+
+        public async Task<IEnumerable<Candidate>> ListByCandidateIdAsync(int candidateId)
+        {
+           return await _candidateRepository.ListByCandidateIdAsync(candidateId);
+        }
+
+        public async Task<CandidateResponse> AssignCandidateAsync(int postId, int candidateId)
+        {
+            try
+            {
+
+                await _candidateRepository.AssignCandidate(postId, candidateId);
+                await _unitOfWork.CompleteAsync();
+                Candidate productTag = await _candidateRepository.FindByPostIdAndCandidateId(postId, candidateId);
+                return new CandidateResponse(productTag);
+            }
+            catch (Exception ex)
+            {
+                return new CandidateResponse($"An error ocurred while assigning Candidate to Post: {ex.Message}");
+            }
+        }
+
+        public async Task<CandidateResponse> UnassignCandidateAsync(int postId, int candidateId)
+        {
+            try
+            {
+                Candidate candidate = await _candidateRepository.FindByPostIdAndCandidateId(postId, candidateId);
+                _candidateRepository.Remove(candidate);
+                await _unitOfWork.CompleteAsync();
+                return new CandidateResponse(candidate);
+            }
+            catch (Exception ex)
+            {
+                return new CandidateResponse($"An error ocurred while unassigning Candidate to Post: {ex.Message}");
+            }
         }
     }
 }
